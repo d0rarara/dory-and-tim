9027720c3be744b16ec1f9f2d6d468f0b3c6187d
d0rarara <123691519+d0rarara@users.noreply.github.com>
2023-01-29 03:43:19 +0000

    add SimpInt class

---
+++ SimpInt.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class SimpInt{
+  public static void main(String[] args){
+    double int_rate = 5.0;
+    int no_of_yrs = 5;
+
+    Scanner myObj = new Scanner (System.in);
+    System.out.println("Enter loan amount: ");
+    double princi_amnt = myObj.nextDouble();
+    System.out.println("The simple interest of the loan is: " + (princi_amnt*int_rate*no_of_yrs)/100 );
+
+    
+  }
+}
