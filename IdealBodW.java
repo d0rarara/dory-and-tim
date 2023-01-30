ee0439fa4cac8801dc7b8663bc0db04858886b60
d0rarara <123691519+d0rarara@users.noreply.github.com>
2023-01-30 02:16:46 +0000

    add IdealBodW class

---
+++ IdealBodW.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class IdealBodW{
+  public static final int feet = 5;
+  public static final int pounds = 110;
+
+  public static void main(String[] args){
+    Scanner myObj = new Scanner(System.in);
+    System.out.println("Enter height in feet: ");
+    int height = myObj.nextInt();
+    System.out.println("Enter inches: ");
+    int inch = myObj.nextInt();
+
+    if(height == feet){
+      System.out.println("Your ideal body weight is: " + ((inch*5)+pounds));
+    } else if(height > feet){
+      System.out.println("Your ideal body weight is: " + (((12*5)+(inch*5))+pounds));
+    }
+  }
+}
