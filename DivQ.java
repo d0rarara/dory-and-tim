139651819b794e8238b9533e5cf234a3d5743a47
d0rarara <dorcaskumbu@outlook.com>
2023-02-12 23:08:25 -0500

    add DivQ class

---
+++ DivQ.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DivQ {
+  public static void main(String[] args) {
+	//program should output the dividend, divisor, quotient, and remainder on the screen
+
+
+    Scanner myObj = new Scanner(System.in);
+    System.out.println("Enter first number: ");
+    int n1 = myObj.nextInt();
+    System.out.println("Enter second number: ");
+    int n2 = myObj.nextInt();
+
+    int quotient = n1 / n2;
+    int remainder = n1 % n2;
+
+    System.out.println(n1 + "/" + n2 + " gives " + quotient + " as a quotient and " + remainder + " as a remainder!");
+  }
+}
